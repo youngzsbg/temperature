aed5437cccf18418e6204086276839e01365bcd5
youngzsbg <k-payne101@hotmail.com>
2024-09-28 14:35:59 -0400

    starting js

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+console.log("Script")
+
+//Creating string variables
+//keyword nameVariable = value;
+
+let myName = "Jane";
+let lastName = "Doe";
+
+console.log(myName);
+console.log(lastName);
+console.log(myName + " " +lastName);//concatenation
+
+//creating numerical variables
+let myAge = 99;
+console.log("Age: " + myAge);
+
+let myGrade = 9.5;
+console.log(myGrade);
+
+//creating boolean variables (true or false)
+let isAdmin =true;
+let isStudent=false;
+let isProfessor=true;
+console.log(isProfessor);
+
+myName="Kendall";
+lastName="Payne"
+//my name is Kendall Payne and I am 99 years old.
+console.log("my name is" + myName + " " +lastName + ", and I am" + myAge +" years old");
+
+let number1 = 10;
+let number2 = 5;
+
+//addition
+let addition = number1 + number2;
+console.log(number1 + " + " + number2 + " = " + addition)
+
+//subtraction
+let subtraction = number1 - number2;
+
+
+
+
+//constants
+//keyword constName = value;
+
+const pi = 3.1416;
+//pi= 4.5;  // we cant modify a constant
+console.log(pi);
+
+// prompt; this is simple way to interact with a user
+
+let pet =prompt(Inser)
